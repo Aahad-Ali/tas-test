a1ef0f791acc8145a2b29ea57aa0b4050d169ca0
AahadAliTAS <aahad@technoholicas.com>
2024-05-27 10:24:40 +0500

    done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,66 +1,84 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export const Home = () => {
 
-    const [data, setData] = useState([]);
-    const [Search, setSearch] = useState('');
+  const [newData, setNewData] = useState([]);
+  const [Search, setSearch] = useState("");
 
+  let API = "https://jsonplaceholder.typicode.com/posts";
+  const fetch1 = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((responce) => {
+        setNewData(responce);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: "GET",
-            headers: { "content-type": "application/json" },
-        }
-        ).then(res => {
-            return res.json();
-        }).then(responce => {
-            setData(responce);
-        })
+  useEffect(() => {
+    fetch1();
+  }, []);
 
+  return (
+    <>
+      <div className="mt-5 pt-5">
+        <input
+          onChange={(value) => setSearch(value.target.value)}
+          placeholder="Search"
+          type="text"
+          className="form-control my-5"
+        />
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            {
+              localStorage.clear();
+              window.location.reload(true);
+            }
+          }}
+        >
+          <LogoutIcon /> log out
+        </button>
+        <div className="container">
+          <div className="row">
+            {newData
+              .filter((filterValue) =>
+                filterValue.title.toLowerCase().includes(Search.toLowerCase())
+              )
 
-    })
-
-    return (
-        <>
-            <div className="mt-5 pt-5">
-                <input
-                    onChange={(value) => setSearch(value.target.value)}
-                    placeholder="Search"
-                    type="text"
-                    className="form-control my-5"
-                />
-                <button onClick={() => {
-
-                    {
-                        localStorage.clear()
-                        window.location.reload(true)
-                    }
-
-
-                }}>log out</button>
-                <div className="container">
-                    <div className="row">
-                        {data.filter((filterValue) => filterValue.title.toLowerCase().includes(Search.toLowerCase()))
-
-                            .map((data) => {
-                                return (
-                                    <div className="col-md-4">
-                                        <div className="card" style={{ width: "18rem", height: "600px", margin: "20px" }}>
-                                            <img src='https://media.licdn.com/dms/image/D5603AQGdCXGNoAjCAg/profile-displayphoto-shrink_200_200/0/1702290181115?e=1707955200&v=beta&t=ImgoK1w9cC6X4mTrretWtHQ8u-p_Vx1hVWQfpn5CWMI' className="card-img-top" alt="..." />
-                                            <div className="card-body">
-                                                <h5 className="card-title">{data.title}</h5>
-                                                <p className="card-text">{data.body}</p>
-                                                <a href="#" className="btn btn-primary">
-                                                    Go somewhere
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })}
+              .map((data) => {
+                return (
+                  <div className="col-md-4">
+                    <div
+                      className="card"
+                      style={{
+                        width: "18rem",
+                        height: "600px",
+                        margin: "20px",
+                      }}
+                    >
+                      <img
+                        src="https://media.licdn.com/dms/image/D5603AQGdCXGNoAjCAg/profile-displayphoto-shrink_200_200/0/1702290181115?e=1707955200&v=beta&t=ImgoK1w9cC6X4mTrretWtHQ8u-p_Vx1hVWQfpn5CWMI"
+                        className="card-img-top"
+                        alt="..."
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">{data.title}</h5>
+                        <p className="card-text">{data.body}</p>
+                        <a href="#" className="btn btn-primary">
+                          Go somewhere
+                        </a>
+                      </div>
                     </div>
-                </div>
-            </div>
-        </>
-    )
-}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
